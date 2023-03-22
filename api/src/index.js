f5b222dd4c337108e8c55f8a0a3ee6f8734838b4
Sora-210 <sokominto1231@gmail.com>
2023-03-22 18:10:11 +0000

    add: api path

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const cors = require('cors')
+const fs = require('fs')
+const crypto = require("crypto");
+
+const mongo = require('mongodb').MongoClient;
 
 //App Instance
 const app = express();
@@ -9,34 +13,50 @@ app.use(express.json({ limit: 2000000 }))
 //Use Cors
 app.use(cors())
 
-// Health
-app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: "ok",
-        message: "Version 1.0"
-    })
-})
+//DB Connect
+const uri = "mongodb://root:root@mongo"
+// mongo.connect(uri, (err, client) => {
+    // console.log(err)
+    // console.log(client)
 
-// Login [Mock] [Id: "sus", Password: "suwarika"]
-app.post('/login', (req, res) => {
-    if (req.body.id && req.body.password) {
-        // Auth Logic
-        if (req.body.id === "sus" && req.body.password === "suwarika") {
+
+    // Health
+    app.get('/health', (req, res) => {
+        res.status(200).json({
+            status: "ok",
+            message: "Version 1.0"
+        })
+    })
+
+    // Login [Mock] [Id: "sus", Password: "suwarika"]
+    app.post('/login', (req, res) => {
+        if (req.body.id && req.body.password) {
+            // Auth Logic
+            if (req.body.id === "sus" && req.body.password === "suwarika") {
+                res.status(200).json({
+                    token: "asdfghjkl"
+                })
+            } else {
+                res.status(401).json({})
+            }
+        } else {
+            res.status(401).json({})
+        }
+    })
+
+    // Upload [AuthPartMock]
+    app.post('/upload', (req, res) => {
+        if (req.body.id && req.body.img && req.body.ocr) {
             res.status(200).json({
-                token: "asdfghjkl"
+                status: 1
             })
         } else {
             res.status(401).json({})
         }
-    } else {
-        res.status(401).json({})
-    }
-})
+    })
 
-// Upload [AuthPartMock]
-
-
-// App start
-app.listen('2222', () => {
-    console.log("API Server Starting...")
-})
+    // App start
+    app.listen('2222', () => {
+        console.log("API Server Starting...")
+    })
+// })
