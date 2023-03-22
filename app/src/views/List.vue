9f412c09e039e00f2ea318564a34193fb1d47059
Sora-210 <sokominto1231@gmail.com>
2023-03-22 17:05:45 +0000

    Add: Create Project

---
+++ app/src/views/List.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <h1>&gt; FileList</h1>
+    <v-divider></v-divider>
+    <v-container class="mt-3">
+      <v-row>
+        <table id="list">
+          <thead>
+            <tr>
+              <th>Action</th>
+              <th>FileName</th>
+              <th>Upload Date</th>
+              <th>COMPANY Name</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="row in lists" :key="row.id" class="my-2">
+              <td><input type="checkbox"></td>
+              <td>{{ row.name }}</td>
+              <td>{{ row.date }}</td>
+              <td>{{ row.company }}</td>
+              <td>
+                <v-chip
+                  small
+                  :color="chips[row.status].color"
+                  outlined
+                >
+                  {{ chips[row.status].text }}
+                </v-chip>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const chips = ref({
+  success: {
+    color: "success",
+    text: "Success"
+  },
+  processing: {
+    color: "deep-purple accent-4",
+    text: "Processing"
+  },
+  comfirm: {
+    color: "warning",
+    text: "Comfirm"
+  },
+  error: {
+    color: "error",
+    text: "Error"
+  },
+})
+
+const lists = ref([
+  {
+    id: "sdfghjukjmnbvfghj",
+    name: "File1",
+    date: "2023/01/01",
+    company: "XYZ",
+    status: "success"
+  },
+  {
+    id: "mnhuikmnbhuikmn",
+    name: "File2",
+    date: "2023/03/01",
+    company: "ABC",
+    status: "processing"
+  },
+  {
+    id: "iuygvbnjhgbnj",
+    name: "File3",
+    date: "2023/03/01",
+    company: "Test",
+    status: "comfirm"
+  },
+  {
+    id: "cdrtghjikaloiuyt",
+    name: "File4",
+    date: "2023/04/01",
+    company: "Enter",
+    status: "error"
+  },
+])
+</script>
+
+<style>
+#list {
+  width: 100%;
+  padding: 5px;
+
+  border: solid 1px #000;
+  border-radius: 5px;
+}
+
+#list thead {
+  padding-bottom: 10px;
+  border-bottom: solid 1px #000;
+}
+
+#list td {
+  text-align: center;
+}
+</style>
---
+++ app/src/views/View.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <h1>> View</h1>
+    <v-divider></v-divider>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h2>{{ fileName }}</h2>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col>
+          <v-btn
+            variant="outlined"
+            color="error"
+            active=false
+          >
+            PDF Download
+          </v-btn>
+          <v-btn
+            variant="outlined"
+            color="success"
+            :active="isCSV"
+          >
+            CSV Download
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-container>
+        <div class="my-2">
+          <h3>Image</h3>
+          <v-row
+            class="my-3"
+          >
+            <v-col cols="6">
+              <v-card
+                outlined
+              >
+                <v-card-title>Original</v-card-title>
+                <img
+                  src="../assets/old.png"
+                  alt="defaultImage"
+                  class="mb-5 document"
+                >
+              </v-card>
+            </v-col>
+            <v-col cols="6">
+              <v-card
+                outlined
+              >
+                <v-card-title>Changing</v-card-title>
+                <img
+                  src="../assets/new.png"
+                  alt="newImage"
+                  class="mb-5 document"
+                >
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
+        <v-divider></v-divider>
+        <div class="my-2">
+          <h3>Datas</h3>
+          <v-row>
+            <table id="list">
+              <thead>
+                <tr>
+                  <th>Key</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>TestKey</td>
+                  <td>TestValue</td>
+                </tr>
+                <!-- <tr v-for="row in lists" :key="row.id" class="my-2">
+                  <td><input type="checkbox"></td>
+                  <td>{{ row.name }}</td>
+                </tr> -->
+              </tbody>
+            </table>
+          </v-row>
+        </div>
+      </v-container>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'View',
+  data() {
+      return {
+          fileName: "Test.png",
+          status: 0,
+          isPDF: false,
+          isCSV: false,
+          data: [
+              {
+                  "key": "testKey",
+                  "value": "testValue"
+              }
+          ]
+      }
+  }
+}
+</script>
+
+<style>
+.document {
+  width: 100%;
+}
+
+#list {
+  width: 100%;
+  padding: 5px;
+
+  border-radius: 5px;
+}
+
+#list thead {
+  padding-bottom: 10px;
+  border-bottom: solid 1px fff;
+}
+
+#list td {
+  text-align: center;
+}
+</style>
